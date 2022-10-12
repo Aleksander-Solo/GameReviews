8f3f9dc29ee8f9df585e592a1d8ae21513f1d648
Olek <oleksolol@wp.pl>
2022-10-12 13:24:06 +0200

    Add project files.

---
+++ GameReviews/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace GameReviews
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int GameId { get; set; }
+    }
+}
---
+++ GameReviews/Game.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameReviews
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Producer { get; set; }
+        public string Description { get; set; }
+        [RangeAttribute(1.0, 10.0, ErrorMessage = "Value for Rating must be between 1.0 and 10.0.")]
+        public float Rating { get; set; }
+        [RangeAttribute(1.0, 10.0, ErrorMessage = "Value for Rating must be between 1.0 and 10.0.")]
+        public float CriticsRating { get; set; }
+        public List<Comment> Comments { get; set; }
+        public byte[] Image;
+        public DateTime Premiere { get; set; }
+    }
+}
